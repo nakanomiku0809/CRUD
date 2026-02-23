2787287cacad89f46a268ccb64f5a0c0c7d09b8c
mikunino <moisesdj@uninorte.edu.co>
2026-02-23 13:31:17 +0000

    clase Usuario y Main

---
+++ Main.java
@@ -0,0 +1,6 @@
+public Class Main {
+    public static void main (String[]args){
+        Usuario user=new Usuario(1,"laura","laura@gmail.com");
+        System.out.println("Usuario creado"+user);
+    }
+}
