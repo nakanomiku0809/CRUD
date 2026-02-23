5d5e818673a9fc583569299dd29700c84166b662
mikunino <moisesdj@uninorte.edu.co>
2026-02-23 13:34:47 +0000

    ajuste

--- Main.java
+++ Main.java
@@ -1,4 +1,4 @@
-public Class Main {
+public class Main {
     public static void main (String[]args){
         Usuario user=new Usuario(1,"laura","laura@gmail.com");
         System.out.println("Usuario creado"+user);
